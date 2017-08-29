e4d28aca924dc002118abfe1a7b83ae0a4b94118
Tomek Buslowski <tbuslowski@gmail.com>
2017-08-29 19:52:21 +0200

    inital

---
+++ iToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iToDo
+//
+//  Created by Tomek Buslowski on 29.08.2017.
+//  Copyright © 2017 Tomek Buslowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
